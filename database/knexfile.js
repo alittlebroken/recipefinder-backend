0cd65651741032ebea883f3e27401429463f6bf5
Paul Lockyer <plockyer@googlemail.com>
2022-07-01 16:57:27 +0100

    Added initial database config

---
+++ database/knexfile.js
@@ -0,0 +1,17 @@
+import('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS,
+      host: process.env.DB_HOST
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+};
