24d0266a9553238e9fbe64cc97fa0e4e476eeaf2
Paul Lockyer <plockyer@googlemail.com>
2022-07-04 13:08:02 +0100

    Added migration step and seeds for recipes

---
+++ database/migrations/20220701205112_recipes.js
@@ -0,0 +1,33 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+   .createTable('recipes', table => {
+     table.increments('id').primary();
+     table.integer('userId').unsigned().notNullable();
+     table.foreign('userId')
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+     table.string('name', 255).notNullable();
+     table.string('description');
+     table.integer('servings').unsigned().notNullable();
+     table.integer('calories_per_serving').unsigned().notNullable();
+     table.integer('prep_time').unsigned().notNullable();
+     table.integer('cook_time').unsigned().notNullable();
+     table.integer('rating');
+     table.timestamps(true, true);
+   });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+   .dropTable('recipes');
+};
---
+++ database/seeds/003_recipes.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('recipes').del()
+  await knex('recipes').insert([
+    {
+      id: 1,
+      userId: 1,
+      name: 'Vegan Chilli',
+      description: 'Our healthy vegan chilli recipe packs in plenty of vegetables and doesn’t fall short on the flavour front. Serve it with rice or in jacket potatoes for a filling supper',
+      servings: 4,
+      calories_per_serving: 367,
+      prep_time: 15,
+      cook_time: 45,
+      rating: 10
+    },
+    {
+      id: 2,
+      userId: 2,
+      name: 'Easy vegan burgers',
+      description: 'Make vegan burgers with polenta and spice with cumin, chilli and coriander. Cook in the oven or on the barbecue and serve with a plant-based salsa',
+      servings: 10,
+      calories_per_serving: 252,
+      prep_time: 20,
+      cook_time: 70,
+      rating: 90
+    }
+  ]);
+};
