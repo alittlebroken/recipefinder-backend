8e36e69b58a1b1d6a09276eeac6a0d47f921148b
Paul Lockyer <plockyer@googlemail.com>
2022-09-23 08:58:38 +0100

    added API return messages to constants file to aid in DRY methology

---
+++ helpers/constants.js
@@ -0,0 +1,19 @@
+/*
+ * Common error and informational messages used by
+ * the API.
+ */
+
+const INFO_RECORD_CREATED = "Record successfully created succesffuly";
+const INFO_RECORD_UPDATED = "Record successfully updated successfully";
+const INFO_RECORD_REMOVED = "Record successfully removed successfully";
+
+const ERROR_MISSING_VALUES = "One or more required values are missing or incorrect";
+const ERROR_GENERIC_RESOURCE = "There was a problem with the resource, please try again later";
+
+module.exports = {
+  INFO_RECORD_CREATED,
+  INFO_RECORD_REMOVED,
+  INFO_RECORD_UPDATED,
+  ERROR_MISSING_VALUES,
+  ERROR_GENERIC_RESOURCE
+}
