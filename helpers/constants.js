97a793001b926c966aa302c13f0e0f7ecb4628a0
Paul Lockyer <plockyer@googlemail.com>
2022-09-23 09:19:04 +0100

    added message when simulating a knex error

--- helpers/constants.js
+++ helpers/constants.js
@@ -9,11 +9,13 @@ const INFO_RECORD_REMOVED = "Record successfully removed successfully";
 
 const ERROR_MISSING_VALUES = "One or more required values are missing or incorrect";
 const ERROR_GENERIC_RESOURCE = "There was a problem with the resource, please try again later";
+const ERROR_SIMULATE = "Lost connection to the database";
 
 module.exports = {
   INFO_RECORD_CREATED,
   INFO_RECORD_REMOVED,
   INFO_RECORD_UPDATED,
   ERROR_MISSING_VALUES,
-  ERROR_GENERIC_RESOURCE
+  ERROR_GENERIC_RESOURCE,
+  ERROR_SIMULATE
 }
