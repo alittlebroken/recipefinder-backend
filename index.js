1de969413010640f56536e1158dfcf58c289501e
Paul Lockyer <plockyer@googlemail.com>
2022-10-04 16:25:58 +0100

    added logging to the app via morgan

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const cors = require('cors');
 const app = express();
 const passport = require('passport');
 const morgan = require('morgan');
+const rfs = require('rotating-file-stream');
+const path = require('path');
+
+/*
+ * Log all errors for the app here
+ */
+const httpErrorLogStream = rfs.createStream('error.log', {
+  interval: '1d',
+  path: path.join(__dirname, 'logs')
+});
+
+/*
+ * Log all access to the app here
+ */
+const accessLogStream = rfs.createStream('access.log', {
+  interval: '1d',
+  path: path.join(__dirname, 'logs')
+});
 
 /*
  * Configure the app
@@ -16,7 +34,22 @@ const morgan = require('morgan');
  app.use(express.urlencoded({ extended: true }));
  app.use(cors());
  app.use(passport.initialize());
- app.use(morgan('combined'));
+
+ app.use(morgan('dev', {
+   stream: httpErrorLogStream,
+   skip: (req, res) => { return res.statusCode < 400 }
+ }));
+
+ app.use(morgan('combined', {
+   stream: accessLogStream
+ }));
+
+ /*
+  Only display logs to the console if in development mode
+ */
+ if(process.env.ENVIRONMENT !== 'production' || process.env.ENVIRONMENT !== 'prod'){
+   app.use(morgan('combined'));
+ }
 
  app.get('/', (req, res) => {
    res.send('Boo!')
