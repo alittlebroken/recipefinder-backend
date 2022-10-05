eb1d858eb847d8ca501563809f011c42073e06d5
Paul Lockyer <plockyer@googlemail.com>
2022-10-05 13:17:27 +0100

    Added custom format for log output and added to logger

--- config/winston.js
+++ config/winston.js
@@ -6,10 +6,19 @@ const winston = require('winston');
 const path = require('path');
 const LOGPATH = path.join('../', process.env.LOG_LOCATION);
 
+/* Create custom format for log output */
+const customFormat = winston.format.printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}]: ${level} - ${message}`;
+});
+
 /* Create the logger for the appliation log */
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.label({ label: process.env.APP_NAME}),
+    winston.format.timestamp({ format: 'DD-MM-YY HH:mm:ss'}),
+    customFormat
+  ),
   transports: [
     new winston.transports.File({
       filename: path.join(LOGPATH, process.env.LOG_APP_COMMON)
@@ -17,12 +26,10 @@ const logger = winston.createLogger({
   ]
 });
 
-/* Add printing to the console if we are not in production */
+/* Add a console transport if we are in development only */
 if(process.env.ENVIRONMENT !== 'production' || process.env.ENVIRONMENT !== 'prod'){
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-};
+  logger.add(new winston.transports.Console())
+}
 
 /*
  * log a message
@@ -31,7 +38,6 @@ const logMessage = (level, message) => {
   logger.log({
     level: level,
     message: message,
-    timestamp: winston.format.timestamp()
   })
 }
 
