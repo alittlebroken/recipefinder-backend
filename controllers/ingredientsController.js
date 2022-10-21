8d27cd51efb59f6aef8bfbb53ec3b40f67527a75
Paul Lockyer <plockyer@googlemail.com>
2022-10-21 20:39:33 +0100

    Added routes, controllers & tests for ingredients

---
+++ controllers/ingredientsController.js
@@ -0,0 +1,310 @@
+/* Add in any modules we need to use */
+require('dotenv').config();
+const appLogger = require('../config/winston');
+
+const ingredientModel = require('../models/ingredientModel');
+
+/*
+ * Returns all the ingredients within the database
+ * 
+*/
+const get = async (req, res, next) => {
+
+    try{
+
+        /* Get the ingredients from the DB */
+        const results = await ingredientModel.findAll();
+
+        if(!results || results.length < 1){
+            res.status(404).json([]);
+        } 
+
+        if(results.success === false){
+            let err = {
+                status: 500,
+                success: false,
+                message: results.message
+            };
+
+            throw err;
+        }
+
+        res.status(200).json(results);
+
+    } catch(e) {
+        return next(e);
+    }
+
+};
+
+/*
+ * Retrieve an individual ingredient from the database based upon it's 
+ * unique identifier
+ */
+const getById = async (req, res, next) => {
+
+    try{
+
+        /* validate any passed in variables */
+        if(!req.params || req.params === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined request parameters'
+            }
+        }
+
+        if(!req.params.id || req.params.id === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined id'
+            }
+        }
+
+        /* Try and get the desired record */
+        let id = parseInt(req.params.id);
+        const result = await ingredientModel.findById(id);
+
+        if(!result || result.length < 1){
+            res.status(404).json([]);
+        }
+
+        res.status(200).json(result);
+
+    } catch(e) {
+        return next(e);
+    }
+
+};
+
+/*
+ * Allows the addition of a new ingredient in the database
+ * 
+ */
+const create = async (req, res, next) => {
+
+    try{
+
+        /* Perform validation of passed in values */
+        if(!req.body || req.body === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined body parameter'
+            }
+        }
+
+
+        if(!req.body.name || req.body.name === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined name'
+            }
+        }
+
+        if(typeof req.body.name !== 'string'){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Wrong name format'
+            }
+        }
+
+        if(req.body.name === ''){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Empty name'
+            }
+        }
+
+        /* Add the record to the database */
+        const result = await ingredientModel.create(req.body.name);
+
+        if(!result || result.length < 1 || result.success === false){
+            throw {
+                status: 500,
+                success: false,
+                message: 'There was a problem with the resource, please try again later'
+            }
+        }
+
+        res.status(200).json(result);
+
+
+    } catch(e) {
+        return next(e);
+    }
+
+};
+
+/*
+ * Removes all ingredients from the database
+ *
+ */
+const remove = async (req, res, next) => {
+
+    try{
+
+        /* remove the data from the database */
+        const result = await ingredientModel.removeAll();
+        
+        if(!result || result.success === false){
+            throw {
+                status: 500,
+                success: false,
+                message: 'There was an issue with the resource, please try again later'
+            }
+        }
+
+        if(result.count < 1){
+            res.status(404).json({ count: 0});
+        }
+
+        res.status(200).json({
+            status: 200,
+            success: true,
+            message: 'All ingredients removed successfully',
+            count: result.count
+        });
+
+    } catch(e) {
+        return next(e);
+    }
+
+};
+
+/*
+ * Removes an ingredient form the system
+ *
+ */
+const removeById = async (req, res, next) => {
+
+    try{
+
+        /* Validate any passed in values */
+        if(!req.params || req.params === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined request parameters'
+            }
+        }
+
+        if(!req.params.id || req.params.id === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined id'
+            }
+        }
+
+        let id = parseInt(req.params.id);
+
+        /* attempt to remove the ingredient by it's id */
+        const result = await ingredientModel.remove(id);
+
+        if(!result || result.success === false){
+            throw {
+                status: 500,
+                success: false,
+                message: 'There was a problem with the resource, please try again later'
+            }
+        }
+
+        if(result.count < 1){
+            res.status(404).json({
+                success: false,
+                count: 0,
+                message: 'There are no ingredients to remove'
+            });
+        }
+
+        res.status(200).json({
+            status: 200,
+            success: true,
+            message: 'Ingredient successfully removed',
+            count: result.count
+        });
+
+    } catch(e) {
+        return next(e);
+    }
+
+};
+
+/* 
+ * Allows you to update an ingredient in the database
+ *
+ */
+const update = async (req, res, next) => {
+
+    try{
+
+        /* validate the values passed via the request object */
+        if(!req.params || req.params === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined request parameters'
+            }
+        }
+
+        if(!req.params.id || req.params.id === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined id'
+            } 
+        }
+
+        if(!req.body || req.body === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined request body'
+            }
+        }
+
+        if(!req.body.name || req.body.name === undefined){
+            throw {
+                status: 400,
+                success: false,
+                message: 'Undefined name'
+            }
+        }
+
+        let id = parseInt(req.params.id);
+        let name = req.body.name;
+
+        /* Update the product */
+        const result = await ingredientModel.update(id, name);
+
+        if(!result || result.success === false){
+            throw {
+                status: 500,
+                success: false,
+                message: 'There was a problem with the resource, please try again later'
+            }
+        }
+        
+        res.status(200).send(result);
+
+    } catch(e) {
+        return next(e);
+    }
+
+};
+
+
+
+module.exports = {
+  get,
+  getById,
+  create,
+  remove,
+  removeById,
+  update
+}
