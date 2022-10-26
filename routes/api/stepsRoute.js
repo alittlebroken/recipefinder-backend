2b568587bec4f8c47498cdca3bfe89601479ab74
Paul Lockyer <plockyer@googlemail.com>
2022-10-26 08:45:10 +0100

    Created routes, controller and tests for steps

--- routes/api/stepsRoute.js
+++ routes/api/stepsRoute.js
@@ -5,10 +5,12 @@ const appLogger = require('../../config/winston');
 const express = require('express');
 const router = express.Router();
 
+const stepsController = require('../../controllers/stepsController');
+
 router.get('/', stepsController.find);
 router.get('/:id', stepsController.findById);
 router.post('/', stepsController.create);
-router.delete('/', stepsController.remove);
+router.delete('/', stepsController.removeAll);
 router.delete('/:id', stepsController.removeById);
 router.put('/:id', stepsController.update);
 
