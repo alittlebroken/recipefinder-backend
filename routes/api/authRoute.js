d7a5194b259560cbaa1cdd273cc7d8ee2de3089f
Paul Lockyer <plockyer@googlemail.com>
2022-12-02 22:06:41 +0000

    enabled route for registering a user

--- routes/api/authRoute.js
+++ routes/api/authRoute.js
@@ -8,6 +8,6 @@ const router = express.Router();
 const authController = require('../../controllers/authController');
 
 router.post('/login', authController.loginUser);
-//router.post('/register', authController.createUser);
+router.post('/register', authController.createUser);
 
 module.exports = router;
