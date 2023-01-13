84be95c4c5191aee93d8fb10f724a5347dbd28ac
Paul Lockyer <plockyer@googlemail.com>
2023-01-13 09:44:19 +0000

    Secured routes for logged in users only

--- routes/api/cookbooksRoute.js
+++ routes/api/cookbooksRoute.js
@@ -3,20 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const cookbookController = require('../../controllers/cookbookController');
+const passport = require('passport');
 
   /* Add the various routes */
-  router.get('/', cookbookController.get);
-  router.get('/:id', cookbookController.getById);
-  router.get('/:id/recipes', cookbookController.recipes);
-  router.get('/:id/category', cookbookController.getCategories);
-  router.post('/', cookbookController.create);
-  router.post('/:id/recipe', cookbookController.addRecipe);
-  router.post('/:id/category', cookbookController.addCategory);
-  router.delete('/', cookbookController.removeAll);
-  router.delete('/:id', cookbookController.removeById);
-  router.delete('/:id/recipes', cookbookController.removeRecipes);
-  router.delete('/:id/categories', cookbookController.removeCategories);
-  router.put('/', cookbookController.update);
+  router.get('/', passport.authenticate('jwt', { session: false }), cookbookController.get);
+  router.get('/:id', passport.authenticate('jwt', { session: false }), cookbookController.getById);
+  router.get('/:id/recipes', passport.authenticate('jwt', { session: false }), cookbookController.recipes);
+  router.get('/:id/category', passport.authenticate('jwt', { session: false }), cookbookController.getCategories);
+  router.post('/', passport.authenticate('jwt', { session: false }), cookbookController.create);
+  router.post('/:id/recipe', passport.authenticate('jwt', { session: false }), cookbookController.addRecipe);
+  router.post('/:id/category', passport.authenticate('jwt', { session: false }), cookbookController.addCategory);
+  router.delete('/', passport.authenticate('jwt', { session: false }), cookbookController.removeAll);
+  router.delete('/:id', passport.authenticate('jwt', { session: false }), cookbookController.removeById);
+  router.delete('/:id/recipes', passport.authenticate('jwt', { session: false }), cookbookController.removeRecipes);
+  router.delete('/:id/categories', passport.authenticate('jwt', { session: false }), cookbookController.removeCategories);
+  router.put('/', passport.authenticate('jwt', { session: false }), cookbookController.update);
 
 
 module.exports = router;
