e271fe2d29a7c1689a06226b4f96dbfa0140c815
Paul Lockyer <plockyer@googlemail.com>
2023-01-18 14:47:30 +0000

    Secured certain routes further with roles

--- __tests__/cookbookController.tests.js
+++ __tests__/cookbookController.tests.js
@@ -17,8 +17,18 @@ describe('cookbookController', () => {
       forename: 'Site',
       surname: 'Administrator',
       roles: ['Admin']
-  }
+    }
     authToken = await userModel.generateToken({user});
+
+     /* User used to trigger a failed authorised middleware check */
+     failUser = {
+      id: 2,
+      email: 'failed@localhost',
+      forename: 'Failed',
+      surname: 'User',
+      roles: ['Sales']
+    }
+    failToken = await userModel.generateToken({ user: failUser});
   });
 
   describe('list', () => {
@@ -109,6 +119,41 @@ describe('cookbookController', () => {
         .get(`/cookbooks/`);
 
       expect(res.status).toEqual(returnStatus);
+  
+    });
+
+    it('should return status 403 if a unauthorised users tries to use this resource', async () => {
+
+      // Set Mocked data that models and controllers should return
+      const modelreturnData = [];
+    
+      // Set Mocked data that models and controllers should return
+      
+      // Mock any needed third party modules
+      jest.spyOn(cookbookModel, 'findAll').mockImplementation(() => {
+        return mockCookbooks;
+      });
+  
+      // Set here the expected return values for the test
+      const returnStatus = 403;
+      const returnSuccess = false;
+      const returnMessage = 'You are not authorized to access the specified route';
+  
+      /* Execute the function */
+      const res = await request(app)
+        .get(`/cookbooks/`)
+        .set('Authorization', `Bearer ${failToken}`);
+  
+      /* Test everything works as expected */
+      expect(res.status).toEqual(returnStatus);
+  
+      expect(typeof res.body).toBe('object');
+      expect(typeof res.body.status).toBe('number');
+      expect(typeof res.body.success).toBe('boolean');
+  
+      expect(res.body.status).toEqual(returnStatus);
+      expect(res.body.success).toEqual(returnSuccess);
+      expect(res.body.message).toEqual(returnMessage);
   
     });
 
@@ -2928,6 +2973,41 @@ describe('cookbookController', () => {
       /* Test everything works as expected */
       expect(res.status).toEqual(returnStatus);
 
+    });
+
+    it('should return status 403 if a unauthorised users tries to use this resource', async () => {
+
+      // Set Mocked data that models and controllers should return
+      const mockModelReturnData = [];
+    
+      // Set Mocked data that models and controllers should return
+      
+      // Mock any needed third party modules
+      jest.spyOn(cookbookModel, 'removeAll').mockImplementation(() => {
+        return mockModelReturnData;
+      });
+  
+      // Set here the expected return values for the test
+      const returnStatus = 403;
+      const returnSuccess = false;
+      const returnMessage = 'You are not authorized to access the specified route';
+  
+      /* Execute the function */
+      const res = await request(app)
+        .delete(`/cookbooks`)
+        .set('Authorization', `Bearer ${failToken}`);
+  
+      /* Test everything works as expected */
+      expect(res.status).toEqual(returnStatus);
+  
+      expect(typeof res.body).toBe('object');
+      expect(typeof res.body.status).toBe('number');
+      expect(typeof res.body.success).toBe('boolean');
+  
+      expect(res.body.status).toEqual(returnStatus);
+      expect(res.body.success).toEqual(returnSuccess);
+      expect(res.body.message).toEqual(returnMessage);
+  
     });
 
     it('should return status 500 if the resource enciunters any other problem', async () => {
--- routes/api/cookbooksRoute.js
+++ routes/api/cookbooksRoute.js
@@ -1,6 +1,8 @@
 /* Require needed modules */
 require('dotenv').config();
 
+const { checkRoles } = require('../../middlewares/verifyMiddleware');
+
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
@@ -8,14 +10,14 @@ const passport = require('passport');
 const cookbookController = require('../../controllers/cookbookController');
 
   /* Add the various routes */
-router.get('/', passport.authenticate('jwt', { session: false }), cookbookController.list);
+router.get('/', passport.authenticate('jwt', { session: false }), checkRoles(['Admin']),cookbookController.list);
 router.get('/:id', passport.authenticate('jwt', { session: false }), cookbookController.getById);
 router.get('/:id/recipes', passport.authenticate('jwt', { session: false }), cookbookController.recipes);
 router.get('/:id/category', passport.authenticate('jwt', { session: false }), cookbookController.getCategories);
 router.post('/', passport.authenticate('jwt', { session: false }), cookbookController.create);
 router.post('/:id/recipe', passport.authenticate('jwt', { session: false }), cookbookController.addRecipe);
 router.post('/:id/category', passport.authenticate('jwt', { session: false }), cookbookController.addCategory);
-router.delete('/', passport.authenticate('jwt', { session: false }), cookbookController.removeAll);
+router.delete('/', passport.authenticate('jwt', { session: false }), checkRoles(['Admin']),cookbookController.removeAll);
 router.delete('/:id', passport.authenticate('jwt', { session: false }), cookbookController.removeById);
 router.delete('/:id/recipes', passport.authenticate('jwt', { session: false }), cookbookController.removeRecipes);
 router.delete('/:id/categories', passport.authenticate('jwt', { session: false }), cookbookController.removeCategories);
