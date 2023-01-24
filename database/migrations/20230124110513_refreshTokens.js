1bd4fcfcd36e2463a0aa3447b82b086102e504be
Paul Lockyer <plockyer@googlemail.com>
2023-01-24 11:09:26 +0000

    added refeshtokens migration

---
+++ database/migrations/20230124110513_refreshTokens.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+     .createTable('refreshtokens', table => {
+       table.increments('id').primary();
+       table.string('username', 255).notNullable();
+       table.string('token', 255).notNullable();
+       table.timestamps(true, true);
+     });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema
+     .dropTable('refreshtokens');
+  };
+  
