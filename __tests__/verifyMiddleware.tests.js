0b134eb044c2f5cd9926ae90383dc80c4b718141
Paul Lockyer <plockyer@googlemail.com>
2023-02-02 09:53:02 +0000

    added missed tests for checkToken

--- __tests__/verifyMiddleware.tests.js
+++ __tests__/verifyMiddleware.tests.js
@@ -1,5 +1,8 @@
 const verify = require('../middlewares/verifyMiddleware');
 
+const userModel = require('../models/userModel');
+const passport = require('passport');
+
 describe('verifyMiddleware', () => {
     
   beforeEach(async () => {
@@ -105,5 +108,141 @@ describe('verifyMiddleware', () => {
     });
 
   });
+
+
+  describe('checkToken', () => {
+
+    beforeEach(() => {
+
+      // Create a new set of env vars for the tests
+      mockReq = {};
+      mockResponse = () => {
+        const response = {};
+        response.status = jest.fn().mockReturnValue(response);
+        response.json = jest.fn().mockReturnValue(response);
+        return response;
+      }
+      mockRes = mockResponse();
+      mockNxt = jest.fn();
+
+      mockTokenUser = {
+        id: 1,
+        roles: 'Customer'
+      }
+
+    })
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    })
+
+
+    it('should successfully check if a token is valid, add user details to the request object and call the next middleware', async () => {
+
+      // setup
+      const passportError = null
+      const passportInfo = null
+      const passportUser = mockTokenUser
+
+      const authType = 'jwt'
+      const options = { session: false }
+
+      passport.authenticate = jest.fn((authType, options, callback) => () => {
+        callback(passportError, passportUser, passportInfo);
+      });
+
+      // execute
+      await verify.checkToken(mockReq, mockRes, mockNxt);
+
+      // assert
+      expect(mockReq.user).toEqual(mockTokenUser)
+      expect(mockNxt).toHaveBeenCalled()
+
+    })
+
+    it('should return status 401 if the token has expired', async () => {
+
+      // setup
+      const passportError = null
+      const passportInfo = { message: 'jwt expired' }
+      const passportUser = undefined
+
+      const authType = 'jwt'
+      const options = { session: false }
+
+      passport.authenticate = jest.fn((authType, options, callback) => () => {
+        callback(passportError, passportUser, passportInfo);
+      });
+
+      // execute
+      await verify.checkToken(mockReq, mockRes, mockNxt);
+
+      // assert
+      expect(mockRes.status).toHaveBeenCalledWith(401)
+      expect(mockRes.json).toHaveBeenCalledWith({
+        status: 401,
+        success: false,
+        message: 'Your access token has expired, please login'
+      })
+
+      expect(mockReq.user).toEqual(passportUser)
+      
+
+    })
+
+    it('should return status 401 if no auth token was set', async () => {
+
+      // setup
+      const passportError = null
+      const passportInfo = { message: 'No auth token' }
+      const passportUser = undefined
+
+      const authType = 'jwt'
+      const options = { session: false }
+
+      passport.authenticate = jest.fn((authType, options, callback) => () => {
+        callback(passportError, passportUser, passportInfo);
+      });
+
+      // execute
+      await verify.checkToken(mockReq, mockRes, mockNxt);
+
+      // assert
+      expect(mockRes.status).toHaveBeenCalledWith(401)
+      expect(mockRes.json).toHaveBeenCalledWith({
+        status: 401,
+        success: false,
+        message: 'You are not authorized to access this resource, please login'
+    })
+
+      expect(mockReq.user).toEqual(passportUser)
+      
+
+    })
+
+    it('should call next with an error for any other message', async () => {
+
+      // setup
+      const passportError = null
+      const passportInfo = { message: 'jwt token format not readable' }
+      const passportUser = undefined
+
+      const authType = 'jwt'
+      const options = { session: false }
+
+      passport.authenticate = jest.fn((authType, options, callback) => () => {
+        callback(passportError, passportUser, passportInfo);
+      });
+
+      // execute
+      await verify.checkToken(mockReq, mockRes, mockNxt);
+
+      // assert
+      expect(mockNxt).toHaveBeenCalledWith(passportInfo.message)
+      expect(mockReq.user).toEqual(undefined)
+      
+    })
+
+  })
 
 })
