9706837dcd1f70ad985a8af5200a21e79bc23ccd
Paul Lockyer <plockyer@googlemail.com>
2023-02-22 22:22:47 +0000

    encrypted initial password for seeded users

--- database/seeds/001_users.js
+++ database/seeds/001_users.js
@@ -1,3 +1,10 @@
+// Bring in any packages needed
+require('dotenv').config();
+const bcrypt  = require('bcrypt')
+
+// Get the password for the SEEDED users
+const hashPass = bcrypt.hashSync(process.env.SEED_PASS, 10)
+
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('users').del()
@@ -6,23 +13,23 @@ exports.seed = async function(knex) {
   await knex('users').insert([
     {
       id: 1,
-      username: 'admin',
-      password: 'admin',
+      username: 'admin@localhost',
+      password: hashPass,
       email: 'admin@localhost',
       forename: 'Site',
       surname: 'Administrator',
       phone_no: '',
-      roles: 'Admin'
+      roles: 'admin'
     },
     {
       id: 2,
-      username: 'user',
-      password: 'user',
-      email: 'user@localhost',
+      username: 'customer@localhost',
+      password: hashPass,
+      email: 'customer@localhost',
       forename: 'Site',
-      surname: 'User',
+      surname: 'Customer',
       phone_no: '',
-      roles: 'User'
+      roles: 'customer'
     }
   ]);
 };
