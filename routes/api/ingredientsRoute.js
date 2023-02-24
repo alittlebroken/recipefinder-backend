b3258bda0cc5c46883d02ae0aa17bdca43fa8892
Paul Lockyer <plockyer@googlemail.com>
2023-02-24 07:59:33 +0000

    added customer role to get all route

--- routes/api/ingredientsRoute.js
+++ routes/api/ingredientsRoute.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const passport = require('passport');
 
 // Specify each route and which controllers they will use
-router.get('/', checkToken, checkRoles(['Admin']),ingredientsController.get);
+router.get('/', checkToken, checkRoles(['Admin','Customer']),ingredientsController.get);
 router.get('/:id', checkToken, ingredientsController.getById);
 router.post('/', checkToken, ingredientsController.create);
 router.delete('/', checkToken, checkRoles(['Admin']),ingredientsController.remove);
