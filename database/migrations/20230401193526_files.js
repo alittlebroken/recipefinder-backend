9cb948d476cf9cd643171c4df180b7f556a07a0b
Paul Lockyer <plockyer@googlemail.com>
2023-04-01 20:52:05 +0100

    created table for file details

---
+++ database/migrations/20230401193526_files.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+     .createTable('files', table => {
+       table.increments('id').primary();
+       table.integer('userid')
+        .unsigned()
+        .notNullable();
+       table.foreign('userid')
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+       table.string('name', 255).notNullable();
+       table.string('mimetype', 255).notNullable();
+       table.string('resource', 255).notNullable();
+       table.integer('resourceid').notNullable();
+       table.timestamps(true, true);
+     });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+     .dropTable('files');
+};
