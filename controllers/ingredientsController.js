507b35ee1183bfe72d7035c73afc82db81ad7b87
Paul Lockyer <plockyer@googlemail.com>
2024-02-19 21:55:32 +0000

    refactored to update instead of creating a new entry each update

--- controllers/ingredientsController.js
+++ controllers/ingredientsController.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const appLogger = require('../config/winston');
 
 const ingredientModel = require('../models/ingredientModel');
+const uploadsModel = require('../models/uploadModel')
+const fs = require('fs')
+const path = require('path')
 
 /*
  * Returns all the ingredients within the database
@@ -319,11 +322,116 @@ const update = async (req, res, next) => {
 
         let id = parseInt(req.params.id);
         let name = req.body.name;
+        
+        /* Remove any existing entries from the files table, but only if we are uploading 
+           a new file*/
+        let removedFiles
+        if(req?.files){
+            removedFiles = await uploadsModel.remove(JSON.stringify({
+                filter: {
+                    resource: 'Ingredients',
+                    resourceid: id
+                }
+            }))
+        }
+
+        /* Extract the params */
+        const files = req.files
+        const userId = req.body.userId ? parseInt(req.body.userId) : parseInt(req.user.id)
+
+        /* The base path for any file uploads */
+        let fileBasePath = `${process.env.APP_URL}/media/`
+
+        /* Set the image source for the update */
+        let imageSource
+        if(req.files && req.files?.length > 0){
+            imageSource = `${fileBasePath}${req?.files[0]?.filename}`
+        } else {
+            imageSource = req?.body?.pictures?.src
+        }
+
+        /* Construct the payload to send to upload method */
+        const payload = [{
+            src: imageSource,
+            mimetype: 'none/none',
+            resource: 'Ingredients',
+            resourceid: parseInt(req?.body?.id),
+            title: req.body?.pictures?.title,
+            userid: parseInt(userId),
+        }]
+
+        /* Update the ingredient record within the files table 
+         * Check that we first have some entries to update, otherwise
+         * we just add a new entry
+         */
+        let existing = await uploadsModel.list({
+            filter: JSON.stringify({
+                resource: 'Ingredients',
+                resourceid: parseInt(req?.body?.id)
+            })
+        })
+
+        if(existing?.results && existing?.results?.length > 0){
+
+            /* Update the existing entry only rather than create a new entry each time */
+            const updatedRecord = await uploadsModel.update({
+                id: existing?.results[0]?.id,
+                src: imageSource,
+                title: req.body?.pictures?.title,
+                alt: req.body?.pictures?.title,
+                mimetype: 'none/none',
+                resource: 'Ingredients',
+                resourceid: parseInt(req?.body?.id),
+                userid: parseInt(userId)
+            },
+            {
+                filter: JSON.stringify({
+                    id: existing?.results[0]?.id
+                })
+            })
+
+            /* Check the return value and act accordingly to the returned
+             * state
+             */
+            if(existing.success === false){
+                throw {
+                    status: 500,
+                    success: false,
+                    message: 'Unable to update the image details for this resource, please try again later.'
+                }
+            }
+
+        } else {
+
+            const result = await uploadsModel.upload(payload)
+
+            /* Check that the upload went OK, if not then we need to remove the file
+            * that was uploaded */
+            if(!result || result.success === false){
+                files.map(file => {
+                    let fullPath = path.join(process.cwd(), '/public/media')
+                    fs.unlink(path.join(fullPath, file.filename), err => {
+                        if (err) throw {
+                            status: 500,
+                            success: false,
+                            message: 'Unable to delete the uploaded files'
+                        }
+                    })
+                })
+        
+                throw {
+                    status: 500,
+                    success: false,
+                    message: 'There was a problem with the resource whilst updating, please try again later',
+                }
+            }
+
+        }
 
         /* Update the product */
-        const result = await ingredientModel.update(id, name);
+        const updateResult = await ingredientModel.update(id, name);
 
-        if(!result || result.success === false){
+        if(!updateResult || updateResult.success === false){
             throw {
                 status: 500,
                 success: false,
@@ -331,7 +439,7 @@ const update = async (req, res, next) => {
             }
         }
         
-        res.status(200).send(result);
+        res.status(200).send(updateResult);
 
     } catch(e) {
         return next(e);
