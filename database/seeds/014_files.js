d50a7849d760b1f6cb0b66e64b33e218ade6a1c6
Paul Lockyer <plockyer@googlemail.com>
2024-02-23 13:56:46 +0000

    set seed images for recipes and users

--- database/seeds/014_files.js
+++ database/seeds/014_files.js
@@ -11,24 +11,19 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('files').del()
   await knex('files').insert([
-    {id:1, src: 'https://xsgames.co/randomusers/avatar.php?g=male', mimetype: 'image/png',resource: 'user', resourceid: 1, userid: 1, title: 'Yummy food 1'},
-    {id:2, src: 'https://xsgames.co/randomusers/avatar.php?g=male', mimetype: 'image/png',resource: 'user', resourceid: 2, userid: 2, title: 'Yummy food 2'},
-    {id:3, src: 'https://xsgames.co/randomusers/avatar.php?g=female', mimetype: 'image/png',resource: 'user', resourceid: 3, userid: 3, title: 'Yummy food 3'},
-    {id: 4, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 1, userid: 1, title: 'Yummy food 4'},
-    {id: 5, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 1, userid: 1, title: 'Yummy food 5'},
-    {id: 6, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 1, userid: 1, title: 'Yummy food 6'},
-    {id: 7, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 2, userid: 2, title: 'Yummy food 7'},
-    {id: 8, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 2, userid: 2, title: 'Yummy food 8'},
-    {id: 9, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 2, userid: 2, title: 'Yummy food 9'},
-    {id: 10, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 3, userid: 1, title: 'Yummy food 10'},
-    {id: 11, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 3, userid: 1, title: 'Yummy food 11'},
-    {id: 12, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 3, userid: 1, title: 'Yummy food 12'},
-    {id: 13, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 4, userid: 1, title: 'Yummy food 13'},
-    {id: 14, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 4, userid: 1, title: 'Yummy food 14'},
-    {id: 15, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 4, userid: 1, title: 'Yummy food 15'},
-    {id: 16, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 5, userid: 2, title: 'Yummy food 16'},
-    {id: 17, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 5, userid: 2, title: 'Yummy food 17'},
-    {id: 18, src: `${faker.image.food()}`, mimetype: 'image/png', resource: 'recipe', resourceid: 5, userid: 2, title: 'Yummy food 18'},
+    {id:1, src: `${process.env.APP_URL}/media/default_user.png`, mimetype: 'image/png',resource: 'user', resourceid: 1, userid: 1, title: 'Default user profile picture'},
+    {id:2, src: `${process.env.APP_URL}/media/default_user.png`, mimetype: 'image/png',resource: 'user', resourceid: 2, userid: 2, title: 'Default user profile picture'},
+    {id:3, src: `${process.env.APP_URL}/media/default_user.png`, mimetype: 'image/png',resource: 'user', resourceid: 3, userid: 3, title: 'Default user profile picture'},
+    {id: 4, src: `${process.env.APP_URL}/media/vegan_chilli.jpg`, mimetype: 'image/jpg', resource: 'recipe', resourceid: 1, userid: 1, title: 'Bowl of Vegan Chilli with garnish'},
+    {id: 5, src: `${process.env.APP_URL}/media/vegan_chilli_two.jpg`, mimetype: 'image/jpg', resource: 'recipe', resourceid: 1, userid: 1, title: 'Pot of vegan chilli on a table'},
+    {id: 6, src: `${process.env.APP_URL}/media/vegan_burger.jpg`, mimetype: 'image/jpg', resource: 'recipe', resourceid: 2, userid: 2, title: 'Two vegan burgers on plates'},
+    {id: 7, src: `${process.env.APP_URL}/media/cheese_on_toast.jpg`, mimetype: 'image/jpg', resource: 'recipe', resourceid: 3, userid: 1, title: 'Slices of bread with melted cheese on top of them arranged in a grid 9x9'},
+    {id: 8, src: `${process.env.APP_URL}/media/meaty_burgers.jpg`, mimetype: 'image/jpg', resource: 'recipe', resourceid: 4, userid: 1, title: '3 Juicy burgers within a bun topped with cheese and bacon'},
+    {id: 9, src: `${process.env.APP_URL}/media/breaded_chicken.jpeg`, mimetype: 'image/jpeg', resource: 'recipe', resourceid: 5, userid: 2, title: 'Plate of braded chicken with dip'},
+    {id: 10, src: `${process.env.APP_URL}/media/spaghetti_bolognese.jpg`, mimetype: 'image/jpg', resource: 'recipe', resourceid: 6, userid: 2, title: 'Bowl of Spaghetti Bolonese on a table'},
+    {id: 11, src: `${process.env.APP_URL}/media/vegetable_soup.jpg`, mimetype: 'image/jpg', resource: 'recipe', resourceid: 7, userid: 2, title: 'Bowl of vegetable soup next to a spoon on a table'},
+    {id: 12, src: `${process.env.APP_URL}/media/pancakes_lemons.jpg`, mimetype: 'image/jpg', resource: 'recipe', resourceid: 8, userid: 2, title: 'Pancake on aplate with slices of lemon and sugar.'},
+    {id: 13, src: `${process.env.APP_URL}/media/pancakes.jpg`, mimetype: 'image/jpg', resource: 'recipe', resourceid: 8, userid: 2, title: 'Folded pancakes on plates'}
   ]);
 
   await knex.raw('select setval(\'files_id_seq\', max(id)) from files');
